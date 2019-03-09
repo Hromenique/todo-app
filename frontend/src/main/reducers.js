6dd201bafc167da9e370b0fd3f2706c508fe70dc
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2019-03-09 00:29:19 -0300

    inclusão da lista em redux e middle para promises

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,26 +1,8 @@
 import { combineReducers } from 'redux'
+import todoReducer from '../todo/todoReducer'
 
 const rootReducer = combineReducers({
-    todo: (state, action) => ({
-        description: 'Ler livro',
-        list: [
-            {
-                _id: 1,
-                description: 'Pagar fatura do cartão',
-                done: true
-            },
-            {
-                _id: 2,
-                description: 'Reunião de equipe',
-                done: false
-            },
-            {
-                _id: 3,
-                description: 'Consulta médica',
-                done: false
-            }
-        ]
-    })
+    todo: todoReducer
 })
 
 export default rootReducer
